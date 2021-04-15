7993cf82837e1ad29985621cc41de89a11f0422e
Pankaj <pkumar20121987@gmail.com>
2021-04-15 19:51:02 +0530

    1st commit

---
+++ src/main/java/com/shalini/com/java/LearnJavaApplication.java
@@ -0,0 +1,13 @@
+package com.shalini.com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shalini/com/java/LearnJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shalini.com.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
